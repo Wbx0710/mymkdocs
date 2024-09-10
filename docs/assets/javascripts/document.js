c275f760031faef4b7b24f54d68cc932e3c2054f
Wbx0710 <3115943441@qq.com>
2024-09-11 07:46:36 +0800

    文件网页跳转

---
+++ docs/assets/javascripts/document.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var links = document.querySelectorAll('a[href^="http"]');
+    links.forEach(function (link) {
+        link.setAttribute('target', '_blank');
+    });
+});
